f69b2cdbb415c610fb3c2498628b5604c778253e
BryanOlivares <49683706+BryanOlivares@users.noreply.github.com>
2019-05-10 09:28:20 -0500

    Add files via upload

---
+++ Nombre.py
@@ -0,0 +1,15 @@
+import turtle
+t=turtle.Pen()
+
+
+t.left(90)
+t.forward(150)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(75)
+t.right(90)
+t.forward(50)
+
+turtle.getscreen()._root.mainloop()
+ 
